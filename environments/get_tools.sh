adc09489f448fd2215863961293092f10c9c71d7
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-08-27 23:49:53 +0200

    chore: Update DevOpsToolkit setup script to include Docker and Ansible installation scripts

--- environments/get_tools.sh
+++ environments/get_tools.sh
@@ -5,31 +5,30 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # Keine Farbe
 
-TOOLS_DIR="$1"   # Verzeichnis, in dem sich die Playbook-Skripte befinden
-TOOLS="$2"       # Liste der ausgewählten Tools (z.B. docker, ansible, terraform)
+TOOLS_DIR="$1"
+TOOLS="$2"
 
 echo -e "${GREEN}Tools directory is: $TOOLS_DIR${NC}"
 echo -e "${GREEN}Selected tools are: $TOOLS${NC}"
 
-# Ansible Playbook ausführen (nur wenn "ansible" im $TOOLS String vorkommt)
-if [[ "$TOOLS" =~ "ansible" ]]; then
-    echo -e "${GREEN}Running Ansible playbook...${NC}"
-    
-    # Überprüfen, ob das Playbook-Skript existiert
-    if [ -f "$TOOLS_DIR/ansible/start_ansible_setup.sh" ]; then
-        bash "$TOOLS_DIR/ansible/start_ansible_setup.sh"
-        
-        # Überprüfen, ob das Playbook erfolgreich ausgeführt wurde
-        if [ $? -eq 0 ]; then
-            echo -e "${GREEN}Ansible playbook executed successfully.${NC}"
-        else
-            echo -e "${RED}Ansible playbook execution failed.${NC}"
-            exit 1
-        fi
+# Überprüfen, welche Tools ausgewählt wurden und die entsprechenden Installationsskripte ausführen
+
+# Docker Installation
+if [[ "$TOOLS" =~ "docker" ]]; then
+    echo -e "${GREEN}Installing Docker...${NC}"
+    if [ -f "$TOOLS_DIR/docker/install_docker.sh" ]; then
+        bash "$TOOLS_DIR/docker/install_docker.sh"
     else
-        echo -e "${RED}Ansible playbook script not found: $TOOLS_DIR/ansible/start_ansible_setup.sh${NC}"
-        exit 1
+        echo -e "${RED}Docker installation script not found: $TOOLS_DIR/docker/install_docker.sh${NC}"
     fi
-else
-    echo -e "${GREEN}Ansible is not listed in the TOOLS variable, skipping playbook execution.${NC}"
+fi
+
+# Ansible Installation
+if [[ "$TOOLS" =~ "ansible" ]]; then
+    echo -e "${GREEN}Installing Ansible...${NC}"
+    if [ -f "$TOOLS_DIR/ansible/install_ansible.sh" ]; then
+        bash "$TOOLS_DIR/ansible/install_ansible.sh"
+    else
+        echo -e "${RED}Ansible installation script not found: $TOOLS_DIR/ansible/install_ansible.sh${NC}"
+    fi
 fi

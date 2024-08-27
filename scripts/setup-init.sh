bc4c25ff0d97112a1b5a4557c849acdef0414526
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-08-27 17:13:26 +0200

    setup-init.sh

---
+++ scripts/setup-init.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Farben für die Ausgabe
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Keine Farbe
+
+# Name des Repositories und des Verzeichnisses
+REPO_URL="https://github.com/NiklasJavier/DevOpsToolkit.git"
+BRANCH="dev"
+CLONE_DIR="/etc/DevOpsToolkit-$BRANCH"
+
+# Überprüfen, ob das Skript als Root ausgeführt wird
+if [ "$EUID" -ne 0 ]; then
+    echo -e "${RED}Please run as root.${NC}"
+    exit 1
+fi
+
+echo -e "${GREEN}Starting the setup...${NC}"
+
+# Überprüfen, ob Git installiert ist
+if ! command -v git &> /dev/null; then
+    echo -e "${RED}Git is not installed. Installing Git...${NC}"
+    sudo apt-get update
+    sudo apt-get install -y git
+
+    # Überprüfen, ob die Installation erfolgreich war
+    if ! command -v git &> /dev/null; then
+        echo -e "${RED}Git installation failed. Aborting...${NC}"
+        exit 1
+    else
+        echo -e "${GREEN}Git installed successfully.${NC}"
+    fi
+else
+    echo -e "${GREEN}Git is already installed.${NC}"
+fi
+
+# Verzeichnis erstellen, wenn es nicht existiert
+if [ ! -d "$CLONE_DIR" ]; then
+    echo -e "${GREEN}Creating directory $CLONE_DIR...${NC}"
+    sudo mkdir -p "$CLONE_DIR"
+fi
+
+# Prüfen, ob das Repository bereits geklont wurde
+if [ -d "$CLONE_DIR/.git" ]; then
+    echo -e "${GREEN}Repository already exists. Pulling latest changes...${NC}"
+    cd "$CLONE_DIR"
+    sudo git pull
+else
+    echo -e "${GREEN}Cloning the repository into $CLONE_DIR...${NC}"
+    sudo git clone -b ${BRANCH} --single-branch "$REPO_URL" "$CLONE_DIR"
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Failed to clone the repository. Aborting...${NC}"
+        exit 1
+    fi
+fi
+
+# Alle Skripte ausführbar machen
+echo -e "${GREEN}Making all scripts in $CLONE_DIR executable...${NC}"
+sudo find "$CLONE_DIR" -type f -name "*.sh" -exec chmod +x {} \;
+
+echo -e "${GREEN}Setup completed! Repository cloned to $CLONE_DIR and scripts are now executable.${NC}"

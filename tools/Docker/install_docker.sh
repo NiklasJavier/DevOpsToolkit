a4d81a8711e47e7f830da844fc87ab29840a1dee
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-08-27 15:53:35 +0200

    update install_docker.sh

--- tools/Docker/install_docker.sh
+++ tools/Docker/install_docker.sh
@@ -23,19 +23,22 @@ curl -fsSL https://get.docker.com -o "$TMP_DIR/get-docker.sh"
 echo -e "${GREEN}Check whether the file is in the Temp folder...${NC}"
 if [ -f "$TMP_DIR/get-docker.sh" ]; then
     echo -e "${GREEN}Docker installation script downloaded to: $TMP_DIR/get-docker.sh ${NC}"
+    echo -e "${GREEN}Installation of Docker...${NC}"
+    sh "$TMP_DIR/get-docker.sh"
+    # Update der Paketliste und Installation der Voraussetzungen
+    echo -e "${GREEN}Installation of Docker-Compose...${NC}"
+    sudo apt-get update
+    sudo apt-get install -y \
+    docker-compose 
+else
+    echo -e "${RED}Docker installation script does not exist at: $TMP_DIR/get-docker.sh ${NC}"
+    echo -e "${RED}Aborting installation.${NC}"
+    exit 1
 fi
-echo -e "${GREEN}Installation of Docker...${NC}"
-sh "$TMP_DIR/get-docker.sh"
 
 # Nach der Nutzung wird der Ordner gelöscht
 echo -e "${GREEN}Cleaning up...${NC}"
 rm -rf "$TMP_DIR"
 echo -e "${GREEN}Temporary directory deleted.${NC}"
-
-# Update der Paketliste und Installation der Voraussetzungen
-echo -e "${GREEN}Installation of Docker-Compose...${NC}"
-sudo apt-get update
-sudo apt-get install -y \
-    docker-compose 
 
 echo -e "${GREEN}Script finished: Docker installation${NC}"

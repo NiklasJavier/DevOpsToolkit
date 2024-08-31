55e77960d3e4eb883813b692da405d794aa6aaad
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-01 01:58:32 +0200

    chore: Update setup_devops_toolkit.sh script to remove unused code, fix typo in URL, and improve tool selection prompt

--- environments/setup_devops_toolkit.sh
+++ environments/setup_devops_toolkit.sh
@@ -13,15 +13,13 @@ printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
 }
 
 _start=1
-_end=100
+_end=15
 
 # Proof of concept
 for number in $(seq ${_start} ${_end})
 do
     sleep 0.1
     ProgressBar ${number} ${_end}
-done
-printf '\nFinished!\n'
 
 # Farben für die Ausgabe
 GREEN='\033[0;32m'
@@ -440,3 +438,6 @@ if [ "$FULL" = true ]; then
         exit 1
     fi
 fi
+
+done
+printf '\nFinished!\n'

b0fefa3f74d15099501a1cfaabf016f02656b006
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-02 00:43:35 +0200

    Refactor cleanup.sh script to improve user deletion and update ansibleName variable

---
+++ scripts/debug/update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Verzeichnis des geclonten Repositories (hier anpassen)
+REPO_DIR="$1/.."    # 
+BRANCH_NAME="dev"  # 
+
+# In das Verzeichnis des Repos wechseln
+cd "$REPO_DIR" || { echo "Fehler: Konnte das Verzeichnis $REPO_DIR nicht finden."; exit 1; }
+
+# Überprüfe den aktuellen Status
+echo "Überprüfe den aktuellen Status des Repositories..."
+git status
+
+# Sicherstellen, dass alle Änderungen committet sind
+echo "Committing alle Änderungen..."
+git add .
+git commit -m "Speichere meine Änderungen" || echo "Keine Änderungen zu committen."
+
+# Hole die neuesten Änderungen vom Remote-Repository
+echo "Hole die neuesten Änderungen vom Remote-Repository..."
+git fetch origin
+
+# Rebase auf die neuesten Änderungen
+echo "Führe rebase auf die neuesten Änderungen durch..."
+git rebase "origin/$BRANCH_NAME"
+
+# Überprüfe auf Konflikte
+if [ $? -ne 0 ]; then
+  echo "Konflikte während des Rebases entdeckt. Bitte behebe die Konflikte und fahre dann fort."
+  exit 1
+fi
+
+echo "Rebase erfolgreich abgeschlossen."

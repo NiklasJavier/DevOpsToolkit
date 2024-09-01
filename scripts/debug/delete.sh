1653eb2a5b77b343a8a0484f89ec698f9b2589bc
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-01 14:25:32 +0200

    chore: Remove unused code and improve error handling in delete.sh script

--- scripts/debug/delete.sh
+++ scripts/debug/delete.sh
@@ -5,3 +5,15 @@ if [ -d /etc/DevOpsToolkit ]; then
 else
     echo "Der Pfad /etc/DevOpsToolkit existiert nicht."
 fi
+
+if [ -d /usr/sbin/devops ]; then
+    rm -r /usr/sbin/devops
+else
+    echo "Der Pfad /usr/sbin/devops existiert nicht."
+fi
+
+if [ -d /var/log/devops_commands.log ]; then
+    rm -r /var/log/devops_commands.log
+else
+    echo "Der Pfad /var/log/devops_commands.log existiert nicht."
+fi

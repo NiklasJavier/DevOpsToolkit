8a4264e4be1f9b8199376e6f5c15cb9a0c139e8b
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-01 23:36:48 +0200

    Refactor cleanup.sh script to update ansibleName variable and remove unnecessary comments

--- scripts/host/cleanup.sh
+++ scripts/host/cleanup.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # Keine Farbe
 tools_dir="$1"      # Tools-Verzeichnis
 config_file="$2"    # Konfigurationsdatei
 
-ansibleName="host_cleanup.yml" # Name des Ansible Playbooks -> playbookname bspw. (local_setup).yml
+ansibleName="host_cleanup" # Name des Ansible Playbooks -> playbookname bspw. (local_setup).yml
 ansibleFolder="host_setup"    # Ordner, in dem das Playbook liegt -> playbookfolder bspw. (local)
 
 bash "$tools_dir/ansible/trigger_ansible_playbook.sh" "$tools_dir" "$config_file" "$ansibleName" "$ansibleFolder"

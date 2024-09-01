a43b200bc67c10daba9e5609c128d6499f7a512c
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-01 17:28:57 +0200

    chore: Update install_tools.sh script to use grey color for informational messages

--- environments/install_tools.sh
+++ environments/install_tools.sh
@@ -2,20 +2,21 @@
 
 # Farben für die Ausgabe
 GREEN='\033[0;32m'
+GREY='\033[1;90m'
 RED='\033[0;31m'
 NC='\033[0m' # Keine Farbe
 
 TOOLS_DIR="$1"
 TOOLS="$2"
 
-echo -e "${GREEN}Tools directory is: $TOOLS_DIR${NC}"
-echo -e "${GREEN}Selected tools are: $TOOLS${NC}"
+echo -e "${GREY}Tools directory is: $TOOLS_DIR${NC}"
+echo -e "${GREY}Selected tools are: $TOOLS${NC}"
 
 # Überprüfen, welche Tools ausgewählt wurden und die entsprechenden Installationsskripte ausführen
 
 # Docker Installation
 if [[ "$TOOLS" =~ "docker" ]]; then
-    echo -e "${GREEN}Installing Docker...${NC}"
+    echo -e "${GREY}Installing Docker...${NC}"
     if [ -f "$TOOLS_DIR/docker/install_docker.sh" ]; then
         bash "$TOOLS_DIR/docker/install_docker.sh"
     else
@@ -25,7 +26,7 @@ fi
 
 # Ansible Installation
 if [[ "$TOOLS" =~ "ansible" ]]; then
-    echo -e "${GREEN}Installing Ansible...${NC}"
+    echo -e "${GREY}Installing Ansible...${NC}"
     if [ -f "$TOOLS_DIR/ansible/install_ansible.sh" ]; then
         bash "$TOOLS_DIR/ansible/install_ansible.sh"
     else

4628a0e793dbcb9fbd01776c193f0753fed564b9
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-01 14:32:47 +0200

    chore: Update traefik.sh script to pass necessary arguments and run Ansible playbook

--- scripts/services/traefik.sh
+++ scripts/services/traefik.sh
@@ -6,26 +6,9 @@ RED='\033[0;31m'
 NC='\033[0m' # Keine Farbe
 
 tools_dir="$1"      # Tools-Verzeichnis
-config_file="$2"
+config_file="$2"    # Konfigurationsdatei
 
-# Variablen für Ansible
 ansibleName="local_setup" # Name des Ansible Playbooks -> playbookname bspw. (local_setup).yml
 ansibleFolder="local"    # Ordner, in dem das Playbook liegt -> playbookfolder bspw. (local)
 
-start_playbook() {
-    local playbookname=$1
-    local playbookfolder=$2
-    echo -e "${GREEN}Running Ansible ${playbookfolder}/${playbookname}...${NC}"
-    ANSIBLE_CONFIG=$tools_dir/ansible/${playbookfolder}/ansible.cfg ansible-playbook -i $tools_dir/ansible/${playbookfolder}/hosts.ini $tools_dir/ansible/${playbookfolder}/playbooks/${playbookname} --extra-vars "CONFIG_YAML=$config_file"
-}
-
-# Überprüfen, ob Docker installiert ist
-if ! command -v docker &> /dev/null
-then
-    echo -e "${RED}Docker ist nicht installiert. Führe Installationsskript aus...${NC}"
-    bash "$tools_dir/docker/install_docker.sh"
-    start_playbook "$ansibleName.yml" "$ansibleFolder"
-else
-    echo -e "${GREEN}Docker ist bereits installiert.${NC}"
-    start_playbook "$ansibleName.yml" "$ansibleFolder"
-fi
+bash "$tools_dir/ansible/trigger_ansible_playbook.sh" "$tools_dir" "$config_file" "$ansibleName" "$ansibleFolder"

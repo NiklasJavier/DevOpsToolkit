4b9ade9a69114ec9d34a00249cebcaf8c5f3ac07
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-02 00:24:22 +0200

    Refactor cleanup.sh script to delete user directories and update ansibleName variable

--- scripts/host/cleanup.sh
+++ scripts/host/cleanup.sh
@@ -3,9 +3,6 @@
 # Aktuellen Benutzernamen definieren, der nicht gelöscht werden soll
 currentUsername="$3"
 
-# Leere Liste zur Speicherung der zu löschenden Benutzer
-user_list=()
-
 # Finde alle Verzeichnisse in /home mit genau 11 Großbuchstaben
 find /home -regextype posix-extended -maxdepth 1 -mindepth 1 -type d -regex '.*/[A-Z]{11}' | while read -r dir; do
   username=$(basename "$dir")
@@ -14,36 +11,13 @@ find /home -regextype posix-extended -maxdepth 1 -mindepth 1 -type d -regex '.*/
   # Füge den Benutzernamen zur Liste hinzu, falls er nicht der currentUsername ist
   if [ "$username" != "$currentUsername" ]; then
     user_list+=("$username")
-    echo "$username wurde der Liste hinzugefügt."
+    echo "$username wird gelöscht"
+    srv_dir="/opt/SRV-$username"
+    home_dir="/home/$username"
+    sudo userdel -r "$username"
+    sudo rm -rf "$srv_dir"
+    sudo rm -rf "$home_dir"
   else
-    echo "Benutzer $username wird nicht zur Liste hinzugefügt, da er der aktuelle Benutzer ist."
-  fi
-done
-
-# Debug-Ausgabe der Liste der zu löschenden Benutzer
-echo "Benutzer, die gelöscht werden sollen:"
-for user in "${user_list[@]}"; do
-  echo "Benutzer in der Liste: $user"
-done
-
-# Benutzer und ihre Verzeichnisse löschen
-for username in "${user_list[@]}"; do
-  echo "Lösche Benutzer: $username"
-  
-  # Benutzer löschen (auskommentiert für den Testlauf)
-  # sudo userdel -r "$username"
-  
-  # Verzeichnis in /opt/SRV-* löschen, wenn es existiert
-  srv_dir="/opt/SRV-$username"
-  if [ -d "$srv_dir" ]; then
-    echo "Lösche Verzeichnis: $srv_dir"
-    # sudo rm -rf "$srv_dir"
-  fi
-
-  # Verzeichnis in /home/* löschen, wenn es existiert
-  home_dir="/home/$username"
-  if [ -d "$home_dir" ]; then
-    echo "Lösche Verzeichnis: $home_dir"
-    # sudo rm -rf "$home_dir"
+    echo "Benutzer $username kann nicht gelöscht werden. Befindet sich in der config.yml"
   fi
 done

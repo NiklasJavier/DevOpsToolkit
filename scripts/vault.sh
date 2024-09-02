67af958573c4411bbd7d5a2897974c67df07272f
Niklas Klein <59505702+NiklasJavier@users.noreply.github.com.>
2024-09-03 01:36:32 +0200

    chore: Refactor debug script to improve systemlink deletion messages

--- scripts/vault.sh
+++ scripts/vault.sh
@@ -5,11 +5,10 @@ RED='\033[0;31m'
 PINK='\033[0;35m'
 GREY='\033[1;90m'
 YELLOW='\033[1;33m' 
-NC='\033[0m' # Keine Farbe
+NC='\033[0m' 
 
-# Zuweisung der Argumente zu Variablen
-vault_file=$4 # Pfad zur Vault-Datei
-vault_secret=$5 # Vault-Passwort
+vault_file=$4 
+vault_secret=$5 
 
 checkIfVaultExists(){
 if [ ! -f "$vault_file" ]; then
@@ -21,18 +20,32 @@ fi
 }
 
 createTemporaryAccessFile(){
-PASS_FILE=$(mktemp)
-echo "$vault_secret" > "$PASS_FILE"
-echo "temp_file: $PASS_FILE"
-chmod 600 "$PASS_FILE"
+if echo "$vault_secret" > "$PASS_FILE"; then
+  echo -e "${GREY}The temporary password file ${YELLOW}$PASS_FILE ${GREY}was successfully created.${NC}"
+  chmod 600 "$PASS_FILE"
+else
+  echo -e "${RED}The temporary password file ${YELLOW}$PASS_FILE ${RED}could not be created.${NC}"
+fi
 }
 
 openVault(){
-ansible-vault edit --vault-password-file="$PASS_FILE" "$vault_file"
+if ansible-vault edit --vault-password-file="$PASS_FILE" "$vault_file"; then
+  echo -e "${GREY}The Vault file ${YELLOW}$vault_file ${GREY}was successfully opened.${NC}" 
+else
+  echo -e "${RED}The Vault file ${YELLOW}$vault_file ${RED}could not be opened.${NC}"
+fi
 }
 
 deleteTemporaryAccessFile(){
 rm -f "$PASS_FILE"
+if [ $? -eq 0 ]; then
+  echo -e "${GREY}The temporary access file ${YELLOW}$PASS_FILE ${GREY}was successfully deleted.${NC}"
+else
+  echo -e "${RED}The temporary access file ${YELLOW}$PASS_FILE ${RED}could not be deleted.${NC}"
+  exit 1
+else
+
+fi
 }
 
 methods=(
